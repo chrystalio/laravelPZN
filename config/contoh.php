dff42c58c9e9d783e40ec4d921b8e6f3f1118d72
Kristoff <chrystalio.k@outlook.com>
2022-07-30 16:09:51 +0700

    Add Contoh.php at config folder

---
+++ config/contoh.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "author" => [
+        "first" => env("FIRST_NAME", "John"),
+        "last" => env("LAST_NAME", "Doe"),
+    ],
+    "email" => "johndoe@example.com",
+    "website" => "https://example.com",
+];
