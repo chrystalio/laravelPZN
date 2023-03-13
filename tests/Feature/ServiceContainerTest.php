63694282ec95cd46ba91ede83670001771a85667
chrystalio <chrystalio.k@outlook.com>
2023-03-13 13:47:44 +0700

    Add bind test case

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Data\Foo;
@@ -17,5 +18,21 @@ class ServiceContainerTest extends TestCase
         self::assertEquals("Foo", $foo1->foo());
         self::assertEquals("Foo", $foo2->foo());
         self::assertNotSame($foo1, $foo2);
+    }
+
+    public function testBind(){
+//        $person = $this->app->make(Person::class);
+//        self::assertNotNull($person);
+
+        $this->app->bind(Person::class, function ($app) {
+            return new Person('Chrystalio', 'Kie');
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals('Chrystalio', $person1->firstName);
+        self::assertEquals('Kie', $person2->lastName);
+        self::assertNotSame($person1, $person2);
     }
 }
